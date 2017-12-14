c5e7fc983b36bd06561d11ad87a81f390670a87e
Doully Yang <themeanyhead@gmail.com>
2017-12-14 21:52:31 +0000

    clean solution

--- LeetCode/maxDepthOfBinaryTree.java
+++ LeetCode/maxDepthOfBinaryTree.java
@@ -8,17 +8,6 @@
  * }
  */
 class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        return maxDepth(root.left, root.right, 1);
-    }
-
-    public int maxDepth(TreeNode node1, TreeNode node2, int max) {
-        if (node1 == null && node2 == null) return max;
-        max++;
-        int maxLeft = node1 != null ? maxDepth(node1.left, node1.right, max) : max;
-        int maxRight = node2 != null ? maxDepth(node2.left, node2.right, max) : max;
-        
-        return Math.max(maxLeft, maxRight);
-    }
+    if(root==null) return 0;
+    return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
 }
