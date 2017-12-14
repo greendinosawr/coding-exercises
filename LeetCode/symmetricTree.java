6ee7ca77b2307b8827fa6e1dc9f366966fe59b81
Doully Yang <themeanyhead@gmail.com>
2017-12-14 21:34:20 +0000

    update cleaner solution

--- LeetCode/symmetricTree.java
+++ LeetCode/symmetricTree.java
@@ -9,19 +9,14 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null || root.left == null && root.right == null) return true;
-        return isSymmetric(root.left, root.right);
+        return isMirror(root, root);
     }
     
-    public boolean isSymmetric(TreeNode node1, TreeNode node2) {
-        if (node1 == null ^ node2 == null || node1.val != node2.val) return false;
-        if (node1.left == null ^ node2.right == null) return false;
-        if (node1.right == null ^ node2.left == null) return false;
-        if (node1.left == null && node1.right == null) return true;
-        
-        if (node1.left == null && node2.right == null) return isSymmetric(node1.right, node2.left);
-        if (node1.right == null && node2.left == null) return isSymmetric(node1.left, node2.right);
-        
-        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
+        return (t1.val == t2.val)
+            && isMirror(t1.right, t2.left)
+            && isMirror(t1.left, t2.right);
     }
 }
